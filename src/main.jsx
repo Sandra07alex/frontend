fa231682bce210ba9400cbd018c9f82d6f2c331f
sandra07alex <sandr5617alex@gmail.com>
2025-12-04 01:52:57 +0530

    fix issus in build

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import UserForm from "./UserForm.jsx";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode><UserForm /></React.StrictMode>
+);
