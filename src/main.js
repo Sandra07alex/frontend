728f3d9be56f35fc5e40e66d6e8778244613c88e
sandra07alex <sandr5617alex@gmail.com>
2025-12-04 14:14:01 +0530

    fix  CORS Header

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import UserForm from "./UserForm";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode><UserForm /></React.StrictMode>
-);
